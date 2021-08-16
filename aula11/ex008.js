3ca63747529cfc9bfac4b35ec69945cb5283e0ca
Breno Gomes <80367915+Brenojgomes@users.noreply.github.com>
2021-08-16 17:03:33 -0300

    condições

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61.0
+console.log(`A velociade do seu carro é ${vel}km/h`)
+if(vel > 60){ // condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO')
+}
+console.log('Dirija sempre usando o cinto de segurança')   
